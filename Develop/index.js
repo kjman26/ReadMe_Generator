65dfb0cb1e09eb61fd9be7a4a2e9bab26f6ad344
Chris <kjman26@gmail.com>
2022-10-17 18:31:13 -0700

    updated index.js to include the questions and prompts

--- Develop/index.js
+++ Develop/index.js
@@ -2,4 +2,56 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const toMarkdown = require("to-markdown");
-const generateMarkdown = require("./Utils/generateMarkdown")
+const generateMarkdown = require("./Utils/generateMarkdown");
+
+//create an array of questions for user input and will go to generateMarkdown
+
+inquirer.createPromptModule([
+    {
+        type: 'input',
+        message: 'ADD Title and a LINK to your project!',
+        name: 'TITLE'
+    },
+    {
+        type: 'input',
+        message: 'Describe your project',
+        name: 'DESCRIPTION'
+    },
+    {
+        type: 'input',
+        message: 'How do you INSTALL your project?',
+        name: 'INSTALL'
+    },
+    {
+        type: 'input',
+        message: 'How do I use the application',
+        name: 'USAGE'
+    },
+    {
+        type: 'list',
+        message: 'What LICENSE/BADGE would you like?',
+        name: 'LICENSE',
+        choices: ["APM", "CRAN/METACRAN", "NPM", "NONE"]
+    },
+    {
+        type: 'input',
+        message: 'Put SHOUTOUTS to contributors here!',
+        name: 'CREDITS'
+    },
+    {
+        type: 'input',
+        message: 'What/Were TESTS were run?',
+        name: "TESTS"
+    },
+    {
+        type: 'input',
+        message: 'Add your GitHub USERNAME',
+        name: 'USERNAME'
+    },
+    {
+        type: 'input',
+        message: 'Add your EMAIL',
+        name: 'EMAIL'
+    }
+]);
+
