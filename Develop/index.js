45e7bd4c543bddaf0daa8ef4e401b23e5d911cba
Chris <kjman26@gmail.com>
2022-10-17 18:04:56 -0700

    added generate markdown js file, added in required packages in index.js

---
+++ Develop/index.js
@@ -0,0 +1,5 @@
+//adding required packages
+const inquirer = require("inquirer");
+const fs = require("fs");
+const toMarkdown = require("to-markdown");
+const generateMarkdown = require("./Utils/generateMarkdown")
